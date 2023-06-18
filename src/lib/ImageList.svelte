f59cd9edd26f2db6d126e04ae748ad20e391f0e4
thorstenbaek <tst@dips.no>
2023-06-18 18:32:52 +0200

    Fixed snappy deletion of image

--- src/lib/ImageList.svelte
+++ src/lib/ImageList.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
+    import {onMount} from "svelte";
     import {sessionStore} from "../stores";
     import ImageTile from "./ImageTile.svelte";
     import { sleep } from "../utils/utils";
 
     const apiKey = "RiCuIUIgCU1fbN2FMBbtaBLY1Bqxjenehb2OJD1zTYAzSeB7WuHZ";
     let doLoadImages = LoadImages($sessionStore.user.email);
+    let images: any[];
+    let status: any;
 
-    export function Refresh() {
-        doLoadImages = LoadImages($sessionStore.user.email);
+    onMount(async () => {
+        await Refresh();
+    });
+
+    export async function Refresh() {
+        images = await LoadImages($sessionStore.user.email);
     }
 
     async function LoadImages(user:string): Promise<any[]> {
-
-        const res = await fetch(`https://book-upload-backend.stenbaek.no/Images?user=${user}`);
-        console.log(res.status);
-        const json = await res.json();
-        console.log(json);
-        return json;
+        try {
+            status = "Loading images..."
+            const res = await fetch(`https://book-upload-backend.stenbaek.no/Images?user=${user}`);
+            console.log(res.status);
+            const json = await res.json();
+            console.log(json);
+            status = ""
+            return json;    
+        } catch (error) {
+            status = error;
+        }
+        
     }
 
     async function DeleteImage(imageId:string) {
@@ -25,16 +38,21 @@
             `https://book-upload-backend.stenbaek.no/Images/Delete?id=${imageId}`, {
                 method: "DELETE"});
         if (res.ok) {
-            await sleep(1000);
+            images = images.filter(i => i.id != imageId);
             console.log(`Successfully deleted image ${imageId}`);
-            Refresh();
         }
     }
     
 </script>
 
 <div>
-    {#await doLoadImages}
+    <p>{status}</p>
+    {#if images}
+        {#each images as image}
+            <ImageTile {image} on:delete={event => DeleteImage(event.detail)}/>
+        {/each}
+    {/if}
+    <!--{#await doLoadImages}
         Laster bilder...
     {:then images} 
         {#each images as image}
@@ -42,5 +60,5 @@
         {/each}
     {:catch error}
         {error}
-    {/await}
+    {/await}-->
 </div>
